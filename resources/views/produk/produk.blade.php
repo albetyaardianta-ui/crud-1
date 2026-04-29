216b29f10e5b2d3d4c4d98160a89a6aa8453e342
albetyaardianta <albetyaardianta@gmail.com>
2026-04-29 18:27:26 +0700

    feat: display product list with currency formatting

--- resources/views/produk/produk.blade.php
+++ resources/views/produk/produk.blade.php
@@ -1,96 +1,56 @@
 @extends('layoutes.main')
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
- 
-@section('content')
-    <div class="container-fluid px-4">
-        <h1 class="mt-4">Dashboard</h1>
-        <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
- 
-        <div class="card mb-4">
-            <div class="card-header">
-                {{-- <i class=""></i> --}}
-                <a href="{{ route('index.create') }}" class="btn btn-sm btn-primary">Tambah data</a>
-            </div>
-            <div class="card-body">
-                <table id="datatablesSimple">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama</th>
-                            <th>Jenis</th>
-                            <th>Harga Jual</th>
-                            <th>Harga Beli</th>
-                            <th>Foto</th>
-                            <th width="280px">Action</th>
-                        </tr>
-                    </thead>
-                    <tfoot>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama</th>
-                            <th>Jenis</th>
-                            <th>Harga Jual</th>
-                            <th>Harga Beli</th>
-                            <th>Foto</th>
-                            <th>Action</th>
-                        </tr>
-                    </tfoot>
-                    <tbody>
-                       @foreach ( $produk as $k )
-<tr>
-    <td>{{ $loop->iteration }}</td>
-    <td>{{ $k->nama }}</td>
-    <td>{{ $k->jenis }}</td>
-    <td>{{ $k->harga_jual }}</td>
-    <td>{{ $k->harga_beli }}</td>
-    <td>
-        @empty($k->foto)
-            <img src="{{url('image/nophoto.jpg')}}" alt="project-image" class="rounded" style="width: 100%; max-width: 100px; height: auto;">
-        @else
-            <img src="{{url('image')}}/{{$k->foto}}" alt="project-image" class="rounded" style="width: 100%; max-width: 100px; height: auto;">
-        @endempty
-    </td>
-    <td>
-        <a href="" class="btn btn-sm btn-secondary">show</a>
-        <a href="{{ route('index.edit', $k->id) }}" class="btn btn-sm btn-warning">edit</a>
-        
-        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal{{$k->id}}">
-            hapus
-        </button>
 
-        <div class="modal fade" id="exampleModal{{$k->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Konfirmasi Hapus</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Apakah anda yakin ingin menghapus produk **{{ $k->nama }}**?
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                        
-                        <form action="{{ route('index.destroy', $k->id) }}" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Ya, Hapus!</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+@section('content')
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Daftar Produk</h1>
+    
+    <div class="card mb-4">
+        <div class="card-header">
+            <a href="{{ route('produk.create') }}" class="btn btn-sm btn-primary">Tambah Data</a>
         </div>
-        </td>
-</tr>
-@endforeach
-                    </tbody>
-                </table>
+        <div class="card-body">
+            @if(session('sucess'))
+            <div class="alert alert-success">
+                {{ session('success') }}
             </div>
+            @endif
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Kategori</th>
+                        <th>Harga Jual</th>
+                        <th>Foto</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+    @foreach($produk as $k)
+    <tr>
+        <td>{{ $loop->iteration }}</td>
+        <td>{{ $k->nama }}</td>
+        <td>{{ $k->kategori->nama ?? 'Tanpa Kategori' }}</td>
+        
+        <td>Rp {{ number_format($k->harga_jual, 0, ',', '.') }}</td>
+        
+        <td>
+            <img src="{{ asset('image/'.$k->foto) }}" width="50" class="rounded">
+        </td>
+        <td>
+            <a href="{{ route('produk.edit', $k->id) }}" class="btn btn-sm btn-warning">Edit</a>
+
+            <form action="{{ route('produk.destroy', $k->id) }}" method="POST" style="display:inline;" onsubmit="return confirm('Yakin ingin menghapus data ini?')">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-sm btn-danger">Hapus</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</tbody>
+            </table>
         </div>
     </div>
+</div>
 @endsection
